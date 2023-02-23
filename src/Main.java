62f8287eb7bd63dfea379dc8384bc2847753b737
Cameliuu <calin.iacob@student.upt.ro>
2023-02-23 18:28:54 +0200

    Read using scanner

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Alex Benciu");
+        int a = scan.nextInt();
+        System.out.println("Ati introdus alexul: "+a);
+
+
+    }
+}
